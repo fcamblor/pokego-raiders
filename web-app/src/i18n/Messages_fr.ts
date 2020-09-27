ec289779042c34958d8eac32d09edd500117cf54
fcamblor <fcamblor@gmail.com>
2020-09-27 11:38:17 +0200

    provided html for registration page

--- web-app/src/i18n/Messages_fr.ts
+++ web-app/src/i18n/Messages_fr.ts
@@ -1,8 +1,40 @@
 
 
 export const TRANSLATIONS = {
+    'teams': {
+        'instinct': {
+            'long': `Intuition (Jaune)`
+        },
+        'valor': {
+            'long': `Bravoure (Rouge)`
+        },
+        'mystic': {
+            'long': `Sagesse (Bleu)`
+        },
+
+    },
     'registration-page': {
-        'title': `Bienvenue sur la page d'inscription {{who}} !`
+        'title': `Bienvenue sur la page d'inscription {{who}} !`,
+        'new-user': `Nouvel utilisateur`,
+        'existing-user': `Utilisateur existant`,
+        'your-ami-code': {
+            'label': `Code AMI`,
+            'description': `important pour qu'on puisse vous inviter facilement`
+        },
+        'your-team': {
+            'label': `Équipe`,
+            'description': ``
+        },
+        'your-nickname': {
+            'label': `Nickname`,
+            'description': `important pour que les hôtes puissent vous retrouver facilement`
+        },
+        'your-level': {
+            'label': `Niveau`
+        },
+        'your-account-token': {
+            'label': `Clef du compte`
+        },
     },
     'communities-list': {
         'title': `Liste des communautés de Raids`
--- web-app/src/pages/registration/registration.page.ts
+++ web-app/src/pages/registration/registration.page.ts
@@ -1,10 +1,14 @@
-import {LitElement, html, customElement} from 'lit-element';
+import {LitElement, html, customElement, css} from 'lit-element';
 import {States} from "../../core/States";
 import {i18n} from "../../directives/i18n";
 
 @customElement('registration-page')
 export class RegistrationPage extends LitElement {
-    static get styles() { return Object.values(window.constructibleStyleSheets); }
+    static get styles() { return Object.values(window.constructibleStyleSheets)
+        // language=css
+        .concat(css`
+        `);
+    }
 
     constructor() {
         super();
@@ -16,7 +20,62 @@ export class RegistrationPage extends LitElement {
 
     render() {
         return html`
-        ${i18n('registration-page.title')}
+    ${i18n('registration-page.title')}
+    <div class="grid-container fluid">
+      <fieldset class="fieldset">
+        <legend>${i18n('registration-page.new-user')}</legend>
+    
+        <div class="grid-container grid-x grid-margin-x fluid">
+          <div class="cell large-4 medium-8">
+            <label>${i18n('registration-page.your-ami-code.label')} <em>(ℹ️ ${i18n('registration-page.your-ami-code.description')})</em>
+                <div class="input-group">
+                    <input type="number" minlength="4" maxlength="4" class="input-group-field text-center" placeholder="0000">
+                    <span class="input-group-label">-</span>
+                    <input type="number" minlength="4" maxlength="4" class="input-group-field text-center" placeholder="0000">
+                    <span class="input-group-label">-</span>
+                    <input type="number" minlength="4" maxlength="4" class="input-group-field text-center" placeholder="0000">
+                    <span class="input-group-label">✅❌</span>
+                </div>
+            </label>
+          </div>
+          <div class="cell large-8 medium-4">
+            <label>${i18n('registration-page.your-team.label')}
+                <div class="input-group">
+                    <select class="input-group-field">
+                      <option value="instinct">${i18n('teams.instinct.long')}</option>
+                      <option value="valor">${i18n('teams.valor.long')}</option>
+                      <option value="mystic">${i18n('teams.mystic.long')}</option>
+                    </select>
+                </div>
+            </label>
+          </div>
+          <div class="cell large-1 medium-2">
+            <label>${i18n('registration-page.your-level.label')}
+                <select>
+                  ${[...Array(40).keys()].reverse().map(n => html`<option value='${n+1}'>${n+1}</option>`)}
+                </select>
+            </label>
+          </div>          
+          <div class="cell large-11 medium-10">
+            <label>${i18n('registration-page.your-nickname.label')} <em>(ℹ️ ${i18n('registration-page.your-nickname.description')})</em>
+              <input type="text">
+            </label>
+          </div>
+        </div>
+      </fieldset>
+      <fieldset class="fieldset">
+        <legend>${i18n('registration-page.existing-user')}</legend>
+    
+        <div class="grid-container grid-x grid-margin-x fluid">
+          <div class="cell">
+            <label>${i18n('registration-page.your-account-token.label')}
+                <textarea rows="3"></textarea>
+            </label>
+          </div>
+        </div>
+      </fieldset>
+    </div>
+        
         <button @click="${this.alert}" type="button" class="success button primary">Hello world !</button>
     `;
     }
